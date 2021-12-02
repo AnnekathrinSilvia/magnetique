563fe6b00bd1d347fd71ab4f52ca73073d93f56f
Ludt <anneludt@Uni-Mainz.De>
2021-12-02 15:09:34 +0100

    Starting to restructure the app

--- app.R
+++ app.R
@@ -8,9 +8,6 @@ MAGNet_DCMvsHCM_igraph <- get(load("MAGNetApp_cloud_data/data/networks/igraph_dc
 MAGNet_DCMvsNFD_igraph <- get(load("MAGNetApp_cloud_data/data/networks/igraph_dcm_vs_nfd.RData"))
 MAGNet_HCMvsNFD_igraph <- get(load("MAGNetApp_cloud_data/data/networks/igraph_hcm_vs_nfd.RData"))
 
-library(shinycssloaders)
-options(spinner.type = 6)
-
 
 # DCM vs HCM --------------------------------------------------------------
 gtl_DCMvsHCM_BP <- GeneTonic_list(
@@ -102,6 +99,10 @@ all_igraph <- list(DCMvsHCM = MAGNet_DCMvsHCM_igraph,
                    DCMvsNFD = MAGNet_DCMvsNFD_igraph,
                    HCMvsNFD = MAGNet_HCMvsNFD_igraph)
 
+all_vst <- list(DCMvsHCM = vst(all_gtls[["DCMvsHCM"]][["BP"]]$dds),
+                DCMvsNFD = vst(all_gtls[["DCMvsNFD"]][["BP"]]$dds),
+                HCMvsNFD = vst(all_gtls[["HCMvsNFD"]][["BP"]]$dds))
+
 rm(MAGNet_DCMvsHCM_GeneTonic,
    MAGNet_DCMvsNFD_GeneTonic,
    MAGNet_HCMvsNFD_GeneTonic)
@@ -132,6 +133,7 @@ library("ggrepel")
 library("igraph")
 library("plotly")
 library("shinycssloaders")
+options(spinner.type = 6)
 
 
 # sourcing external files -------------------------------------------------
@@ -149,9 +151,9 @@ magnetique_ui <- shinydashboard::dashboardPage(
   
   header = shinydashboard::dashboardHeader(title = "magnetique"),
   # header = bs4Dash::bs4DashNavbar(
-    # controlbarIcon = icon("cogs")
+  # controlbarIcon = icon("cogs")
   # ),
-
+  
   # sidebar definition ------------------------------------------------------
   sidebar = shinydashboard::dashboardSidebar(
     title = "Options",
@@ -177,8 +179,8 @@ magnetique_ui <- shinydashboard::dashboardPage(
                 selected = "z_score")
     
   ),
-    
-
+  
+  
   # body definition ---------------------------------------------------------
   body = shinydashboard::dashboardBody(
     tabBox(
@@ -193,7 +195,7 @@ magnetique_ui <- shinydashboard::dashboardPage(
         )
       ),
       shiny::tabPanel(
-        title = "DE!", icon = icon("heartbeat"), value = "tab-de",
+        title = "Gene View", icon = icon("heartbeat"), value = "tab-gene-view",
         fluidRow(
           column(
             width = 5,
@@ -205,53 +207,43 @@ magnetique_ui <- shinydashboard::dashboardPage(
               plotlyOutput("de_volcano")
             )
           )
-        )
-      ),
-      shiny::tabPanel(
-        title = "Enrichment map!", icon = icon("project-diagram"), value = "tab-emap",
-        fluidRow(
-          column(
-            width = 8,
-            withSpinner(
-              visNetworkOutput("visnet_em")
-            ) # ,
-            # withSpinner(
-            #   visNetworkOutput("visnet_ggs")
-            # )
-          ),
-          column(
-            width = 4,
-            plotOutput("emap_signature")
-          )
-        )
-      ),
-      shiny::tabPanel(
-        title = "DTU!", icon = icon("flask"), value = "tab-dtu",
+        ),
         fluidRow(
           column(
             width = 12,
             selectizeInput(
-                "gene_name", "Choose one gene:", choices = NULL
-              ),
+              "gene_name", "Choose one gene:", choices = NULL
+            ),
             withSpinner(
               plotOutput("dtu_plot")
-              ),
-            tableOutput("dtu_table")
+            )
           )
         )
       ),
       shiny::tabPanel(
-        title = "Carnival!", icon = icon("sitemap"), value = "tab-carnival",
+        title = "Geneset View", icon = icon("project-diagram"), value = "tab-geneset-view",
         fluidRow(
           column(
-            width = 8,
+            width = 4,
+            DT::dataTableOutput("de_table2")
+          ),
+          column(
+            width = 7,
             withSpinner(
-              visNetworkOutput("visnet_igraph")
+              plotOutput("enriched_funcres")
             )
+          )
+        ),
+        fluidRow(
+          column(
+            width = 5,
+            withSpinner(
+              visNetworkOutput("visnet_em")
+            ) 
           ),
           column(
             width = 4,
-            plotOutput("carnival_counts")
+            plotOutput("emap_signature")
           )
         )
       ),
@@ -277,8 +269,9 @@ magnetique_server <- function(input, output, session) {
   rvalues <- reactiveValues()
   rvalues$mygtl <- NULL
   rvalues$myigraph <- NULL
+  rvalues$myvst <- NULL
   
-
+  
   # selector of gtl object --------------------------------------------------
   rvalues$mygtl <- reactive({
     
@@ -286,12 +279,14 @@ magnetique_server <- function(input, output, session) {
     message(input$selected_ontology)
     
     all_gtls[[input$selected_contrast]][[input$selected_ontology]]
-    # all_gtls[["DCMvsHCM"]][["BP"]]
   })
   
   rvalues$myigraph <- reactive({
     all_igraph[[input$selected_contrast]]
-    # all_igraph[["DCMvsHCM"]]
+  })
+  
+  rvalues$myvst <- reactive({
+    all_vst[[input$selected_contrast]]
   })
   
   # DE related content ---------------------------------------------------------
@@ -304,7 +299,9 @@ magnetique_server <- function(input, output, session) {
     myde <- myde[c("SYMBOL", "log2FoldChange", "padj")]
     DT::datatable(myde, escape = FALSE, options = list(scrollX = TRUE))  %>% 
       DT::formatRound(columns=c('log2FoldChange', 'padj'), digits=3)
-  })
+  },
+  selection = "single")
+  
   
   output$de_volcano <- renderPlotly({
     mygtl <- rvalues$mygtl()
@@ -314,6 +311,17 @@ magnetique_server <- function(input, output, session) {
   })
   
   # enrichment map related content ---------------------------------------------
+  output$de_table2 <- DT::renderDataTable({
+    mygtl <- rvalues$mygtl()
+    myde <- mygtl$res_de
+    myde <- GeneTonic::deseqresult2df(myde)
+    ensembl_url <- "https://www.ensembl.org/Homo_sapiens/Gene/Summary?g="
+    rownames(myde) <- lapply(rownames(myde), function(x) format_url(ensembl_url, x))
+    myde <- myde[c("SYMBOL", "log2FoldChange", "padj")]
+    DT::datatable(myde, escape = FALSE, options = list(scrollX = TRUE))  %>% 
+      DT::formatRound(columns=c('log2FoldChange', 'padj'), digits=3)
+  })
+  
   emap_graph <- reactive({
     emg <- enrichment_map(
       gtl = rvalues$mygtl(),
@@ -321,14 +329,11 @@ magnetique_server <- function(input, output, session) {
       overlap_threshold = 0.1,
       scale_edges_width = 200,
       color_by = input$color_by
-      # color_by = "z_score"
     )
-    # rank_gs <- rank(V(emg)$name)
-    # emg <- permute.vertices(emg, rank_gs)
     return(emg)
   })
   
-
+  
   output$visnet_em <- renderVisNetwork({
     visNetwork::visIgraph(emap_graph()) %>%
       visOptions(
@@ -355,7 +360,7 @@ magnetique_server <- function(input, output, session) {
     # if (!is.null(input$exp_condition)) {
     # message(cur_gsid)
     gs_heatmap(
-      se = vst(rvalues$mygtl()$dds) ,
+      se = rvalues$myvst() ,
       gtl = rvalues$mygtl(),
       geneset_id = cur_gsid,
       FDR = 0.05,
@@ -366,21 +371,14 @@ magnetique_server <- function(input, output, session) {
       scale_row = TRUE,
       anno_col_info = "Etiology"
     )
-    # } else {
-    #   gs_heatmap(
-    #     myvst,
-    #     res_de,
-    #     res_enrich,
-    #     annotation_obj = annotation_obj,
-    #     geneset_id = cur_gsid,
-    #     FDR = input$de_fdr,
-    #     de_only = FALSE,
-    #     cluster_rows = TRUE,
-    #     cluster_columns = TRUE,
-    #     center_mean = TRUE,
-    #     scale_row = TRUE
-    #   )
-    # }
+  })
+  
+  output$enriched_funcres <- renderPlot({
+    gtl <- rvalues$mygtl()
+    enhance_table(gtl$res_enrich, gtl$res_de,
+                  annotation_obj = gtl$annotation_obj,
+                  n_gs = input$number_genesets
+    )
   })
   
   
@@ -398,63 +396,57 @@ magnetique_server <- function(input, output, session) {
     
   })
   
-  output$dtu_table <- renderTable({
-    req(input$gene_name)
-    gtf_gene <- subset(gtf, type == "gene" & gene_name == input$gene_name)
-    results_table(mcols(gtf_gene)[["gene_id"]], se) 
-    
-  })
   
   # carnival-related content ---------------------------------------------------
-  output$carnival_counts <- renderPlot({
-    mygtl <- rvalues$mygtl()
-    
-    g <- rvalues$myigraph()
-    cur_sel <- input$visnet_igraph_selected
-    cur_node <- match(cur_sel, V(g)$name)
-    cur_nodetype <- V(g)$nodetype[cur_node]
-    # validate(need(cur_nodetype == "Feature",
-    #               message = "" # "Please select a gene/feature."
-    # ))
-    
-    validate(need(cur_sel != "",
-                  message = "Please select a node from the graph to plot the expression values."
-    ))
-    
-    cur_geneid <- mygtl$annotation_obj$gene_id[match(cur_sel, mygtl$annotation_obj$gene_name)]
-    
-    message(length(cur_sel))
-    message(cur_geneid)
-    
-    genes_exp <- rownames(mygtl$dds)
-    validate(need(cur_geneid %in% genes_exp,
-                  message = "gene not found in expression matrix" 
-    ))
-    
-    gene_plot(gtl = mygtl, gene = cur_geneid, 
-              intgroup = "Etiology")
-  })
+  # output$carnival_counts <- renderPlot({
+  #    mygtl <- rvalues$mygtl()
+  #   
+  #  g <- rvalues$myigraph()
+  # cur_sel <- input$visnet_igraph_selected
+  #cur_node <- match(cur_sel, V(g)$name)
+  #cur_nodetype <- V(g)$nodetype[cur_node]
+  # validate(need(cur_nodetype == "Feature",
+  #               message = "" # "Please select a gene/feature."
+  # ))
+  
+  #validate(need(cur_sel != "",
+  #              message = "Please select a node from the graph to plot the expression values."
+  #))
+  
+  #cur_geneid <- mygtl$annotation_obj$gene_id[match(cur_sel, mygtl$annotation_obj$gene_name)]
+  
+  #message(length(cur_sel))
+  #message(cur_geneid)
+  
+  #  genes_exp <- rownames(mygtl$dds)
+  # validate(need(cur_geneid %in% genes_exp,
+  #              message = "gene not found in expression matrix" 
+  #))
+  
+  #gene_plot(gtl = mygtl, gene = cur_geneid, 
+  #          intgroup = "Etiology")
+  #})
   
   
-  output$visnet_igraph <- renderVisNetwork({
-    
-    visNetwork::visIgraph(rvalues$myigraph()) %>%
-      visOptions(
-        highlightNearest = list(
-          enabled = TRUE,
-          degree = 1,
-          hover = TRUE
-        ),
-        nodesIdSelection = TRUE
-      ) %>%
-      visHierarchicalLayout(levelSeparation = 100, nodeSpacing = 500,
-        shakeTowards = "leaves") %>% # same as visLayout(hierarchical = TRUE) 
-      visExport(
-        name = "igraph",
-        type = "png",
-        label = "Save igraph graph"
-      )
-  })
+  # output$visnet_igraph <- renderVisNetwork({
+  
+  #    visNetwork::visIgraph(rvalues$myigraph()) %>%
+  #     visOptions(
+  #      highlightNearest = list(
+  #       enabled = TRUE,
+  #      degree = 1,
+  #     hover = TRUE
+  #  ),
+  #  nodesIdSelection = TRUE
+  #) %>%
+  #visHierarchicalLayout(levelSeparation = 100, nodeSpacing = 500,
+  #                      shakeTowards = "leaves") %>% # same as visLayout(hierarchical = TRUE) 
+  #visExport(
+  #  name = "igraph",
+  #  type = "png",
+  #  label = "Save igraph graph"
+  #)
+  # })
   
   # Other content --------------------------------------------------------------
   output$de_volcano_signature <- renderPlot({
@@ -476,9 +468,3 @@ magnetique_server <- function(input, output, session) {
 
 # Launching magnetique! --------------------------------------------------------
 shinyApp(magnetique_ui, magnetique_server)
-
-# same for the diff exp things (but they are anyway in the GTL)
-
-# do compute the z score or so for the res_enrich
-
-## and then have some emap interactive/ggs interactive on that? we need then a numericinput for the nr of genesets
