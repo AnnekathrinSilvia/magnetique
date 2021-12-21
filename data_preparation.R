34ed3611eb95001aaba957a33a0e5b6913ae6774
Ludt <anneludt@Uni-Mainz.De>
2021-12-21 14:05:52 +0100

    Extracted data loading and preparation to separate file and adapted equal colors in each plot (issue #6)

---
+++ data_preparation.R
@@ -0,0 +1,139 @@
+MAGNet_DCMvsHCM_GeneTonic <- readRDS("MAGNetApp_cloud_data/data/DGE/MAGNet_DCMvsHCM_GeneTonic.rds")
+MAGNet_DCMvsNFD_GeneTonic <- readRDS("MAGNetApp_cloud_data/data/DGE/MAGNet_DCMvsNFD_GeneTonic.rds")
+MAGNet_HCMvsNFD_GeneTonic <- readRDS("MAGNetApp_cloud_data/data/DGE/MAGNet_HCMvsNFD_GeneTonic.rds")
+MAGNet_DCMvsHCM_igraph <- get(load("MAGNetApp_cloud_data/data/networks/igraph_dcm_vs_hcm.RData"))
+MAGNet_DCMvsNFD_igraph <- get(load("MAGNetApp_cloud_data/data/networks/igraph_dcm_vs_nfd.RData"))
+MAGNet_HCMvsNFD_igraph <- get(load("MAGNetApp_cloud_data/data/networks/igraph_hcm_vs_nfd.RData"))
+
+
+# DCM vs HCM --------------------------------------------------------------
+gtl_DCMvsHCM_BP <- GeneTonic_list(
+  dds = MAGNet_DCMvsHCM_GeneTonic$dds,
+  res_de = MAGNet_DCMvsHCM_GeneTonic$res_de[order(MAGNet_DCMvsHCM_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_DCMvsHCM_GeneTonic$res_enrich$BP,
+  annotation_obj = MAGNet_DCMvsHCM_GeneTonic$annotation_obj
+)
+
+gtl_DCMvsHCM_MF <- GeneTonic_list(
+  dds = MAGNet_DCMvsHCM_GeneTonic$dds,
+  res_de = MAGNet_DCMvsHCM_GeneTonic$res_de[order(MAGNet_DCMvsHCM_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_DCMvsHCM_GeneTonic$res_enrich$MF,
+  annotation_obj = MAGNet_DCMvsHCM_GeneTonic$annotation_obj
+)
+
+gtl_DCMvsHCM_CC <- GeneTonic_list(
+  dds = MAGNet_DCMvsHCM_GeneTonic$dds,
+  res_de = MAGNet_DCMvsHCM_GeneTonic$res_de[order(MAGNet_DCMvsHCM_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_DCMvsHCM_GeneTonic$res_enrich$CC,
+  annotation_obj = MAGNet_DCMvsHCM_GeneTonic$annotation_obj
+)
+
+
+# DCM vs NFD --------------------------------------------------------------
+gtl_DCMvsNFD_BP <- GeneTonic_list(
+  dds = MAGNet_DCMvsNFD_GeneTonic$dds,
+  res_de = MAGNet_DCMvsNFD_GeneTonic$res_de[order(MAGNet_DCMvsNFD_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_DCMvsNFD_GeneTonic$res_enrich$BP,
+  annotation_obj = MAGNet_DCMvsNFD_GeneTonic$annotation_obj
+)
+
+gtl_DCMvsNFD_MF <- GeneTonic_list(
+  dds = MAGNet_DCMvsNFD_GeneTonic$dds,
+  res_de = MAGNet_DCMvsNFD_GeneTonic$res_de[order(MAGNet_DCMvsNFD_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_DCMvsNFD_GeneTonic$res_enrich$MF,
+  annotation_obj = MAGNet_DCMvsNFD_GeneTonic$annotation_obj
+)
+
+gtl_DCMvsNFD_CC <- GeneTonic_list(
+  dds = MAGNet_DCMvsNFD_GeneTonic$dds,
+  res_de = MAGNet_DCMvsNFD_GeneTonic$res_de[order(MAGNet_DCMvsNFD_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_DCMvsNFD_GeneTonic$res_enrich$CC,
+  annotation_obj = MAGNet_DCMvsNFD_GeneTonic$annotation_obj
+)
+
+
+# HCM vs NFD --------------------------------------------------------------
+gtl_HCMvsNFD_BP <- GeneTonic_list(
+  dds = MAGNet_HCMvsNFD_GeneTonic$dds,
+  res_de = MAGNet_HCMvsNFD_GeneTonic$res_de[order(MAGNet_HCMvsNFD_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_HCMvsNFD_GeneTonic$res_enrich$BP,
+  annotation_obj = MAGNet_HCMvsNFD_GeneTonic$annotation_obj
+)
+
+gtl_HCMvsNFD_MF <- GeneTonic_list(
+  dds = MAGNet_HCMvsNFD_GeneTonic$dds,
+  res_de = MAGNet_HCMvsNFD_GeneTonic$res_de[order(MAGNet_HCMvsNFD_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_HCMvsNFD_GeneTonic$res_enrich$MF,
+  annotation_obj = MAGNet_HCMvsNFD_GeneTonic$annotation_obj
+)
+
+gtl_HCMvsNFD_CC <- GeneTonic_list(
+  dds = MAGNet_HCMvsNFD_GeneTonic$dds,
+  res_de = MAGNet_HCMvsNFD_GeneTonic$res_de[order(MAGNet_HCMvsNFD_GeneTonic$res_de$padj), ],
+  res_enrich = MAGNet_HCMvsNFD_GeneTonic$res_enrich$CC,
+  annotation_obj = MAGNet_HCMvsNFD_GeneTonic$annotation_obj
+)
+
+all_gtls <- list(
+  DCMvsHCM = list(
+    BP = gtl_DCMvsHCM_BP,
+    MF = gtl_DCMvsHCM_MF,
+    CC = gtl_DCMvsHCM_CC
+  ),
+  DCMvsNFD = list(
+    BP = gtl_DCMvsNFD_BP,
+    MF = gtl_DCMvsNFD_MF,
+    CC = gtl_DCMvsNFD_CC
+  ),
+  HCMvsNFD = list(
+    BP = gtl_HCMvsNFD_BP,
+    MF = gtl_HCMvsNFD_MF,
+    CC = gtl_HCMvsNFD_CC
+  )
+)
+
+all_igraph <- list(DCMvsHCM = MAGNet_DCMvsHCM_igraph,
+                   DCMvsNFD = MAGNet_DCMvsNFD_igraph,
+                   HCMvsNFD = MAGNet_HCMvsNFD_igraph)
+
+all_vst <- list(DCMvsHCM = vst(all_gtls[["DCMvsHCM"]][["BP"]]$dds),
+                DCMvsNFD = vst(all_gtls[["DCMvsNFD"]][["BP"]]$dds),
+                HCMvsNFD = vst(all_gtls[["HCMvsNFD"]][["BP"]]$dds))
+
+se <- readRDS("MAGNetApp_cloud_data/data/summarized_experiment.RDS")
+
+
+rm(MAGNet_DCMvsHCM_GeneTonic,
+   MAGNet_DCMvsNFD_GeneTonic,
+   MAGNet_HCMvsNFD_GeneTonic)
+
+rm(gtl_DCMvsHCM_BP,
+   gtl_DCMvsHCM_MF,
+   gtl_DCMvsHCM_CC,
+   gtl_DCMvsNFD_BP,
+   gtl_DCMvsNFD_MF,
+   gtl_DCMvsNFD_CC,
+   gtl_HCMvsNFD_BP,
+   gtl_HCMvsNFD_MF,
+   gtl_HCMvsNFD_CC)
+
+rm(MAGNet_DCMvsHCM_igraph,
+   MAGNet_DCMvsNFD_igraph,
+   MAGNet_HCMvsNFD_igraph)
+
+
+prepareDataTable <- function(se, res_de, contrast){
+  name <- paste0("DRIMSeq_", contrast)
+  dtu_info <- rowData(se)[[name]]
+  names <- rownames(res_de)
+  genes_dtu <- unique(dtu_info$gene_id)
+  
+  dtu_usage <- sapply(names, function(x) if(x %in% genes_dtu){dtu_info[dtu_info$gene_id %in% x,][1,]$lr}else{NA})
+  dtu_padj <- sapply(names, function(x) if(x %in% genes_dtu){dtu_info[dtu_info$gene_id %in% x,][1,]$adj_pvalue}else{NA})
+  df <- data.frame(symbol = res_de$SYMBOL,
+                   log2 = res_de$log2FoldChange,
+                   padj = res_de$padj,
+                   dtuusage = dtu_usage,
+                   dtupadj = dtu_padj)
+  return(df)
+}
