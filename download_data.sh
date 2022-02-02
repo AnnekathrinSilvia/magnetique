43f10cfbe72c8d8584da3e0210d6f6aed8923f3d
Thiago Britto-Borges <tbrittoborges@dieterichlab.org>
2022-02-02 16:40:37 +0100

    remove file after unzip

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 wget --output-document data.zip 'https://data.dieterichlab.org/s/3gCTLGT4DaAqaqb/download' --header 'Accept: text/html' --header 'Connection: keep-alive'
 unzip -o data.zip
+rm data.zip
