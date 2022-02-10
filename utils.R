8763fb6141b30c6058d7d1cbb70c11d3a20977a5
Thiago Britto-Borges <tbrittoborges@dieterichlab.org>
2022-02-10 16:46:04 +0100

    change data loading to psql

--- utils.R
+++ utils.R
@@ -12,7 +12,6 @@ with_tooltip <- function(value, tooltip) {
             title = tooltip, value)
 }
 
-
 #' Compute the mean proportion difference between groups
 get_gid2name <- function(gtf) {
   setNames(
@@ -60,27 +59,6 @@ compute_usage_dif <- function(gene, dataset, .type = "fit_full") {
 #' @param gene selection
 #' @param dataset SummarizedExperiment object with the dataset
 #' @export
-gene_counts <- function(gene, dataset = se) {
-  ind <- which(rowData(dataset)[, "gene_id"] == gene)
-
-  x <- SummarizedExperiment::assays(dataset)[["counts"]][ind, ]
-  x["transcript_id"] <- rownames(x)
-
-  x <- suppressMessages(reshape2::melt(x))
-
-  colnames(x) <- c("transcript_id", "sample_id", "value")
-
-  x["group"] <- SummarizedExperiment::colData(dataset)[
-    x[["sample_id"]], "Etiology"
-  ]
-
-  return(x)
-}
-
-#' Get gene expression for `gene`
-#' @param gene selection
-#' @param dataset SummarizedExperiment object with the dataset
-#' @export
 gene_proportions <- function(gene, dataset, .type = "fit_full") {
   ind <- which(rowData(dataset)[, "gene_id"] == gene)
   x <- SummarizedExperiment::assays(dataset)[[.type]][ind, ]
@@ -97,70 +75,17 @@ gene_proportions <- function(gene, dataset, .type = "fit_full") {
   return(x)
 }
 
-#' Get gene expression for `gene`
-#' @param gene selection
-#' @param dataset SummarizedExperiment object with the dataset
-#' @param gtf GRanges with gene structure
-#' @export
-gene_structure <- function(gene, gtf, dataset) {
-  x <- BiocGenerics::subset(gtf, gene_id %in% c(gene))
-  x <- BiocGenerics::subset(x, transcript_id %in% rownames(dataset))
-  x$type <- plyr::revalue(
-    x$type, c("five_prime_utr" = "utr", "three_prime_utr" = "utr")
-  )
-  x <- split(x, x$transcript_id)
-  return(x)
+plot_gene_structure <- function(gtf) {
+    gtf$type <- plyr::revalue(
+      gtf$type, c("five_prime_utr" = "utr", "three_prime_utr" = "utr")
+    )
+    p <- ggplot() +
+      geom_alignment(gtf,
+        fill = "black", cds.rect.h = .3, utr.rect.h = .2,
+        exon.rect.h = .2, label = T
+      ) +
+      theme_minimal(20) +
+      theme(plot.margin = margin())
+    return(p)
 }
 
-#' Get gene expression for `gene`
-#' @param gene selection
-#' @param dataset SummarizedExperiment object with the dataset
-#' @param gtf GRanges with gene structure
-#' @export
-plot_dtu <- function(gene, dataset, gtf) {
-  library(patchwork)
-  geneid2name <- setNames(
-    nm = gtf$gene_id,
-    gtf$gene_name
-  )
-
-  x_tx <- gene_proportions(gene, dataset)
-  x_structure <- gene_structure(gene, gtf, dataset)
-
-  p2 <- x_tx %>%
-    ggplot() +
-    geom_jitter(aes(x = transcript_id, y = value, color = group),
-      position = position_jitterdodge(),
-      alpha = 0.9, size = 2, show.legend = T, na.rm = TRUE
-    ) +
-    geom_boxplot(aes(x = transcript_id, y = value, fill = group),
-      outlier.size = 0, alpha = 0.4, lwd = 0.5, show.legend = F
-    ) +
-    scale_fill_manual(name = "Etiology", values = group_colors) +
-    scale_colour_manual(name = "Etiology", values = group_colors) +
-    coord_flip() +
-    labs(y = "transcript proportion") + 
-    theme_minimal(20) +
-    theme(
-      axis.title.y = element_blank()
-    )
-  
-
-  p3 <- suppressMessages({
-    ggplot() +
-    ggbio::geom_alignment(x_structure,
-      fill = "black", cds.rect.h = .3, utr.rect.h = .2,
-      exon.rect.h = .2, label = T
-    ) +
-    theme_minimal(20) +
-    theme(plot.margin = margin())
-    })
-
-  p_final <- (p3 | p2) +
-    plot_annotation(
-      theme = theme(plot.margin = margin()),
-      title = stringr::str_glue("{geneid2name[gene]} - {gene}"),
-      caption = "Source: MAGNet / GRCh38.96"
-    )
-  return(p_final)
-}
