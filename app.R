2c4f8d9a37f761bfb293f7e8bd0366df45247060
Federico Marini <marinif@uni-mainz.de>
2022-05-03 10:36:02 +0200

    hiding and showing menu items context-dependent manner, with shinyjs to keep track of the selected values

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ library(promises)
 plan(multisession)
 
 library(shinycssloaders)
+library(shinyjs)
 library(plotly, warn.conflicts = FALSE)
 library(reactable, warn.conflicts = FALSE)
 library(bs4Dash, warn.conflicts = FALSE)
@@ -34,6 +35,7 @@ magnetique_ui <- shinydashboard::dashboardPage(
   # body definition ---------------------------------------------------------
   body = dashboardBody(
     introjsUI(),
+    shinyjs::useShinyjs(),
     shiny::tags$script(
       HTML(
         "$(function(){
@@ -314,23 +316,27 @@ magnetique_server <- function(input, output, session) {
           ),
           selected = "DCMvsHCM"
         ),
-        selectInput("selected_ontology",
-          label = "Ontology",
-          choices = c("BP", "MF", "CC"),
-          selected = "BP"
-        ),
-        numericInput("number_genesets",
-          "Number of gene sets",
-          value = 15,
-          min = 0
-        ),
-        selectInput("color_by",
-          "Color by",
-          choices = c(
-            "z_score",
-            "gs_pvalue"
-          ),
-          selected = "z_score"
+        shinyjs::hidden(
+          tagList(
+            selectInput("selected_ontology",
+                        label = "Ontology",
+                        choices = c("BP", "MF", "CC"),
+                        selected = "BP"
+            ),
+            numericInput("number_genesets",
+                         "Number of genesets",
+                         value = 15,
+                         min = 0
+            ),
+            selectInput("color_by",
+                        "Color by",
+                        choices = c(
+                          "z_score",
+                          "gs_pvalue"
+                        ),
+                        selected = "z_score"
+            )
+          )
         ),
         actionButton("bookmarker",
           label = "Bookmark", icon = icon("heart"),
@@ -339,6 +345,19 @@ magnetique_server <- function(input, output, session) {
       )
     )
   })
+  
+  observeEvent(input$magnetique_tab, {
+    if (input$magnetique_tab == "tab-geneset-view") {
+      shinyjs::show("selected_ontology")
+      shinyjs::show("number_genesets")
+      shinyjs::show("color_by")
+    } else {
+      shinyjs::hide("selected_ontology")
+      shinyjs::hide("number_genesets")
+      shinyjs::hide("color_by")
+    }
+  })
+  
   # selector trigger data loading
   # rvalues$mygtl <- reactive({
   #   message(input$selected_contrast)
