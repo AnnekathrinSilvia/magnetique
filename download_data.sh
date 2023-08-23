d0c4c5056d7c56b4e6d5f7b5739567d1f940cd1f
tbrittoborges <Thiago.BrittoBorges@uni-heidelberg.de>
2023-08-23 12:14:24 +0200

    cleanup

--- download_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget --output-document data.zip 'https://data.dieterichlab.org/s/3gCTLGT4DaAqaqb/download' --header 'Accept: text/html' --header 'Connection: keep-alive'
-unzip -o data.zip
-rm data.zip
